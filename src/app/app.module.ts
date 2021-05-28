d808d282f563ecb2011b4eebe6a472ea96a3b360
Ignis <limadmitri@protonmail.com>
2021-05-28 11:28:07 -0300

    informativo 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EducativoComponent } from './educativo/educativo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducativoComponent
   ],
   imports: [
     BrowserModule,
