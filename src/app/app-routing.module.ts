57b4060185895560f57bac72a4874004509a90eb
Niashi <mathias.pn7@gmail.com>
2021-05-28 15:10:01 -0300

    Ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
-
-const routes: Routes = [
-
-  {path:'' , redirectTo: 'login' , pathMatch: 'full'},
-
-  {path:'login' , component: LoginComponent}  
-=======
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EducativoComponent } from './educativo/educativo.component';
 
 const routes: Routes = [
 
-  
-  {path:'cadastrar',component:CadastrarComponent}
->>>>>>> 75ba4ecc6180122305972b608b8e8ce63414a587
+  {path:'' , redirectTo: 'educativo' , pathMatch: 'full'},
 
-];
+  {path:'login' , component: LoginComponent},
+
+  {path:'cadastrar',component:CadastrarComponent},
+
+  {path:'educativo' , component: EducativoComponent}
+] 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
