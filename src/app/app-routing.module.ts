faf186b9fbc0bd7e1bd3a88fd39919dda4e77829
Ignis <limadmitri@protonmail.com>
2021-05-30 17:20:33 -0300

    textos/imagens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,7 @@
-  
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { CadastrarComponent } from './cadastrar/cadastrar.component';
-import { EducativoComponent } from './educativo/educativo.component';
 
-const routes: Routes = [
-
-  {path:'' , redirectTo: 'educativo' , pathMatch: 'full'},
-
-  {path:'login' , component: LoginComponent},
-
-  {path:'cadastrar',component:CadastrarComponent},
-
-  {path:'educativo' , component: EducativoComponent}
-] 
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
