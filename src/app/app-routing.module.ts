e6f43767da3e01ffe206418ff50c813671ea17d2
Ignis <limadmitri@protonmail.com>
2021-05-30 12:30:05 -0300

    educativo textos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+  
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EducativoComponent } from './educativo/educativo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'' , redirectTo: 'educativo' , pathMatch: 'full'},
+
+  {path:'login' , component: LoginComponent},
+
+  {path:'cadastrar',component:CadastrarComponent},
+
+  {path:'educativo' , component: EducativoComponent}
+] 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
